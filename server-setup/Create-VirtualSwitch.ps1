0df2ac08347916c50d023d26a9bd7aa7126b57f5
Jessica DeVriese <jessdev@umich.edu>
2018-05-11 20:08:26 -0400

    added a virtual switch creation script

---
+++ server-setup/Create-VirtualSwitch.ps1
@@ -0,0 +1,32 @@
+<#
+
+.SYNOPSIS
+    This is a simple Powershell script to create an external virtual switch
+
+.DESCRIPTION
+    The script will try to create a virtual switch using the first physical network adapter it can find.
+
+.PARAMETER SwitchName
+    the name of the virtual switch
+
+.EXAMPLE
+    C:\> ./Create-VirtualSwitch -SwitchName 'MyFavoriteSwitch01'
+
+.NOTES
+    Must have Hyper-V compatible machine to work
+
+.LINK
+http://kevinpelgrims.wordpress.com
+
+#>
+
+
+Param(
+  [string]
+  $SwitchName
+)
+
+$physicalAdapterList = Get-NetAdapter -Physical
+$adapter = $physicalAdapterList[0]
+
+New-VMSwitch -Name $SwitchName -AllowManagementOS $true -NetAdapterName $adapter.Name
